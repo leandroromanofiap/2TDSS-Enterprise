bcfd472f8fad38b5b3ca87038f50840528f13c3b
Leandro Romano <rm75244@fiap.com.br>
2018-02-19 22:45:02 -0300

    Updates

--- 03-JPA-Introducao/src/br/com/fiap/jpa/teste/Teste.java
+++ 03-JPA-Introducao/src/br/com/fiap/jpa/teste/Teste.java
@@ -7,7 +7,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import br.com.fiap.entity.Aluno;
 import br.com.fiap.entity.Curso;
+import br.com.fiap.entity.Periodo;
 import br.com.fiap.entity.StatusCurso;
 
 public class Teste {
@@ -17,16 +19,14 @@ public class Teste {
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("oracle");
 		EntityManager em = factory.createEntityManager();
 		
-		//Aluno aluno = new Aluno("Leandro Romano", "TDS", new GregorianCalendar(2000, Calendar.OCTOBER, 2), true, Periodo.NORTUNO, 3, null);
-		
-//		em.getTransaction().begin();
-//		em.persist(aluno);
-//		em.getTransaction().commit();
-		
+		Aluno aluno = new Aluno("Leandro Romano", "TDS", new GregorianCalendar(2000, Calendar.OCTOBER, 2), true, Periodo.NORTUNO, 3, null);
 		Curso curso = new Curso("Sistemas de Informação", StatusCurso.ATIVO, 3, new GregorianCalendar(2000, Calendar.FEBRUARY, 2), null);
 		
 		em.getTransaction().begin();
+		
 		em.persist(curso);
+		em.persist(aluno);
+		
 		em.getTransaction().commit();
 		
 		em.close();
