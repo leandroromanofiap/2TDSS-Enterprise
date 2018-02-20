86999653e33a29e99cf6a17ec4845642288a9c5c
Leandro Romano <rm75244@fiap.com.br>
2018-02-19 22:55:24 -0300

    Updates

--- 03-JPA-Introducao/src/br/com/fiap/jpa/teste/Teste.java
+++ 03-JPA-Introducao/src/br/com/fiap/jpa/teste/Teste.java
@@ -15,23 +15,25 @@ import br.com.fiap.entity.StatusCurso;
 public class Teste {
 
 	public static void main(String[] args) {
-		
+
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("oracle");
 		EntityManager em = factory.createEntityManager();
-		
-		Aluno aluno = new Aluno("Leandro Romano", "TDS", new GregorianCalendar(2000, Calendar.OCTOBER, 2), true, Periodo.NORTUNO, 3, null);
-		Curso curso = new Curso("Sistemas de Informação", StatusCurso.ATIVO, 3, new GregorianCalendar(2000, Calendar.FEBRUARY, 2), null);
-		
+
+		Aluno aluno = new Aluno("Leandro Romano", "TDS", new GregorianCalendar(2000, Calendar.OCTOBER, 2), true,
+				Periodo.NORTUNO, 3, null);
+		Curso curso = new Curso("Sistemas de Informação", StatusCurso.ATIVO, 3,
+				new GregorianCalendar(2000, Calendar.FEBRUARY, 2), null);
+
 		em.getTransaction().begin();
-		
+
 		em.persist(curso);
 		em.persist(aluno);
-		
+
 		em.getTransaction().commit();
-		
+
 		em.close();
 		factory.close();
-		
+
 	}
-	
+
 }
